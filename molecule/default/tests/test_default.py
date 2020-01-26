d738417bd6774d46bb648fa9cb5fab9ecfb0feee
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:24:40 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,21 +6,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-PYTHON3_BINARY_PATH = '/usr/bin/python3'
-PYTHON3_PACKAGE = 'python3'
-
-
 def test_python3_package_installed(host):
-    host.package("PYTHON3_PACKAGE").is_installed
+    assert host.package("python3").is_installed
 
 
 def test_python3_binary_exists(host):
-    host.file('PYTHON3_BINARY_PATH').exists
+    assert host.file('/usr/bin/python3').exists
 
 
 def test_python3_binary_file(host):
-    host.file('PYTHON3_BINARY_PATH').is_file
+    assert host.file('/usr/bin/python3').is_file
 
 
-def test_python3_binary_whereis(host):
-    host.check_output('whereis python3') == PYTHON3_BINARY_PATH
+def test_python3_binary_which(host):
+    assert host.check_output('whereis python3') == '/usr/bin/python3'
