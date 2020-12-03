83529bf4924864d0c600c2a0b12be42a995a6d23
darkwizard242 <am900820@gmail.com>
2020-12-03 13:43:20 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,18 +6,35 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE1 = 'python3'
+PACKAGE2 = 'python36'
+PACKAGE_BINARY = '/usr/bin/python3'
+
+
 def test_python3_package_installed(host):
-    assert host.package("python3").is_installed or \
-      host.package("python36").is_installed
+    """
+    Tests if python3/python36 is installed.
+    """
+    assert host.package(PACKAGE1).is_installed or \
+        host.package(PACKAGE2).is_installed
 
 
 def test_python3_binary_exists(host):
-    assert host.file('/usr/bin/python3').exists
+    """
+    Tests if python3 binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_python3_binary_file(host):
-    assert host.file('/usr/bin/python3').is_file
+    """
+    Tests if python3 binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_python3_binary_which(host):
-    assert host.check_output('which python3') == '/usr/bin/python3'
+    """
+    Tests the output to confirm python3's binary location.
+    """
+    assert host.check_output('which python3') == PACKAGE_BINARY
